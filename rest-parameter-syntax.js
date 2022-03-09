fe7d1a887ba25574a3cfa0c87bd3e3f42f4ca24c
Abhisek Joshi <nofursad@gmail.com>
2022-03-09 17:59:17 +0000

    Final commit after updates

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,22 @@
  */
 
 // Regular function call 
+ const sumAll = (a, b, c) => a + b + c;
+ let sum = sumAll(1, 2, 3);
+ console.log(sum);
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log(sum2);
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    rest.forEach(item => {
+        sum += item;
+    })
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log(sum3);
